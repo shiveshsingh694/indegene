62f0389eeb52d1a8e76bc6468edc86c50a90dfb2
Shivesh Singh <shiveshsingh694@gmail.com>
2020-09-09 21:22:16 +0530

    add all file

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("#header").prepend(
+    '<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>'
+  );
+  $("#menu-icon").on("click", function () {
+    $("nav").slideToggle();
+    $(this).toggleClass("active");
+  });
+  //your code here
+});
